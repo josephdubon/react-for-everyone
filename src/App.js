0fd57ca193856fa1b6eb039783fab70492dda5c3
Joseph Dubon <josephdubon@pm.me>
2022-02-28 09:22:18 -0800

    default and destructured props examples

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     </div>);
 }
 
-function HelloWorld(props) {
-    return (<h1>Hello, {props.name}</h1>)
+function HelloWorld({name, greeting = 'Hello'}) {
+    return (<h1>{greeting}, {name}</h1>)
 }
 
 export default App;
