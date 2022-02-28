108dcd139a2b3b5ba7a781ad50c0d9a875a17df6
Joseph Dubon <josephdubon@pm.me>
2022-02-28 10:08:32 -0800

    created input field example and added to main app.js below accordion example

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import PropTypes from 'prop-types';
 
-import {Counter} from "./Counter";
-import {Accordion} from "./Accordion";
+import {Counter} from './Counter'
+import {Accordion} from './Accordion'
+import {Input} from './Input'
 
 function App() {
     return (<div className="App">
         <header className="App-header">
             <p>Counter Example</p>
             <Counter/>
+
             <p>Accordion Example</p>
             <Accordion/>
+
+            <p>Input Example</p>
+            <Input/>
         </header>
-    </div>);
+    </div>)
 }
 
 function HelloWorld({name, greeting = 'Hello'}) {
@@ -23,4 +28,4 @@ HelloWorld.propTypes = {
     name: PropTypes.string, greeting: PropTypes.string,
 };
 
-export default App;
+export default App
