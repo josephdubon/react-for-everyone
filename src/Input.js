03504b7a848957286e7de96f3acbe9d42328022b
Joseph Dubon <josephdubon@pm.me>
2022-02-28 10:08:09 -0800

    created input field example and added to main app.js below accordion example

---
+++ src/Input.js
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+
+export function Input() {
+    const [inputValue, setInputValue] = useState('')
+
+    return (<div>
+        <input
+            value={inputValue}
+            onChange={(e) => {
+                setInputValue(e.target.value)
+            }}/>
+    </div>)
+}
