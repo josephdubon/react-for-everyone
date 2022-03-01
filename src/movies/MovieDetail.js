21ff286fae126396ededae08209ce7e1f22f77a4
Joseph Dubon <josephdubon@pm.me>
2022-03-01 15:59:45 -0800

    installed useParams and setup for movie id

--- src/movies/MovieDetail.js
+++ src/movies/MovieDetail.js
@@ -1,7 +1,9 @@
+import {useParams} from 'react-router-dom'
+
 export function MovieDetail() {
-    return (
-        <div>
-            <h1>Movie Detail</h1>
-        </div>
-    )
+    const {id} = useParams()
+
+    return (<div>
+            <h1>Movie Detail {id}</h1>
+        </div>)
 }
