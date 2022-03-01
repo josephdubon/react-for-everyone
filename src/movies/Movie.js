6cf55b3e7b327166af961f67b5728c7912f99d18
Joseph Dubon <josephdubon@pm.me>
2022-03-01 15:17:14 -0800

    added className to link element

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export function Movie({movie, config}) {
     return (
         <li className='movie-item'>
-            <Link to='/movie-detail'>
+            <Link className='App-link' to='/movie-detail'>
                 {config.images?.base_url && (
                     <img
                         src={config.images.base_url + 'w342' + movie.poster_path}
