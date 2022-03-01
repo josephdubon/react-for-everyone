19e29c932e57f1ebc0333f21e4b7daf0cf0428d2
Joseph Dubon <josephdubon@pm.me>
2022-03-01 14:02:33 -0800

    installed react router and added some routes for home and components

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Route, Routes,} from "react-router-dom";
+import {Accordion} from "./Accordion";
+import {Counter} from "./Counter";
+import {MoviesList} from "./movies/MovieLists";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<App/>}/>
+                <Route path="accordion" element={<Accordion/>}/>
+                <Route path="counter" element={<Counter/>}/>
+                <Route path="movies-list" element={<MoviesList/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
