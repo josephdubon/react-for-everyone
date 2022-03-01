de5ff9fdb19295ecb942a705c99106879bf37474
Joseph Dubon <josephdubon@pm.me>
2022-03-01 13:03:48 -0800

    removed poster from title and added a class to the li

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -2,17 +2,14 @@ import PropTypes from 'prop-types'
 
 export function Movie({movie, config}) {
     return (
-        <>
-            <li>
-                {config.images?.base_url && (
-                    <img
-                        src={config.images.base_url + 'w342' + movie.poster_path}
-                        alt={movie.title}/>
-                )}
-                <h3>{movie.title + 'Poster'}</h3>
-            </li>
-
-        </>
+        <li className='movie-item'>
+            {config.images?.base_url && (
+                <img
+                    src={config.images.base_url + 'w342' + movie.poster_path}
+                    alt={movie.title}/>
+            )}
+            <h5>{movie.title}</h5>
+        </li>
     )
 }
 
