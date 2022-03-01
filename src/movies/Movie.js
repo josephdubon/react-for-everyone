531b7179fd90d0384b3601dd700ce79870e6ad12
Joseph Dubon <josephdubon@pm.me>
2022-03-01 12:26:08 -0800

    added poster image to li output

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -2,7 +2,17 @@ import PropTypes from 'prop-types'
 
 export function Movie({movie}) {
     return (
-        <li>{movie.title}</li>
+        <>
+            <li>
+                {config.images?.base_url && (
+                    <img
+                        src={config.images.base_url + 'w342' + movie.poster_path}
+                        alt={movie.title}/>
+                )}
+                <h3>{movie.title + 'Poster'}</h3>
+            </li>
+
+        </>
     )
 }
 
