3a5fbcf513670fc712f3f88616baa94c047e185b
Joseph Dubon <josephdubon@pm.me>
2022-03-01 15:58:32 -0800

    added params to movie detail url

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export function Movie({movie, config}) {
     return (
         <li className='movie-item'>
-            <Link className='App-link' to='/movie-detail'>
+            <Link className='App-link' to={`/movie/${movie.id}`}>
                 {config.images?.base_url && (
                     <img
                         src={config.images.base_url + 'w342' + movie.poster_path}
