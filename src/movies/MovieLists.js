e642643511880d5c97bd1d2ab762102823e125fa
Joseph Dubon <josephdubon@pm.me>
2022-03-01 12:27:35 -0800

    created API call with new function getConfig, added to useEffect

--- src/movies/MovieLists.js
+++ src/movies/MovieLists.js
@@ -2,11 +2,13 @@ import {useEffect, useState} from 'react'
 import {Movie} from "./Movie";
 import {Filter} from "../Filter";
 
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=<API_KEY_HERE>&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=66fc13ab589d1d905e3a3546e4bbc9f2&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
+const CONFIG_URL = 'https://api.themoviedb.org/3/configuration?api_key=66fc13ab589d1d905e3a3546e4bbc9f2'
 
 export function MoviesList() {
     const [filter, setFilter] = useState('')
     const [movies, setMovies] = useState([])
+    const [config, setConfig] = useState([])
 
     const getMovies = async () => {
         try {
@@ -18,7 +20,18 @@ export function MoviesList() {
         }
     }
 
+    const getConfig = async () => {
+        try {
+            const res = await fetch(CONFIG_URL)
+            const config = await res.json()
+            setConfig(config)
+        } catch (e) {
+            console.error(e)
+        }
+    }
+
     useEffect(() => {
+        getConfig()
         getMovies()
     }, [])
 
