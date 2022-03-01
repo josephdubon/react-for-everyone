1f96b7cfbffa662ba192adef22f0117c986e204e
Joseph Dubon <josephdubon@pm.me>
2022-03-01 15:15:48 -0800

    added Link to li content, points to movie-detail page

--- src/movies/Movie.js
+++ src/movies/Movie.js
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom'
 
 export function Movie({movie, config}) {
     return (
         <li className='movie-item'>
-            {config.images?.base_url && (
-                <img
-                    src={config.images.base_url + 'w342' + movie.poster_path}
-                    alt={movie.title}/>
-            )}
-            <h5>{movie.title}</h5>
+            <Link to='/movie-detail'>
+                {config.images?.base_url && (
+                    <img
+                        src={config.images.base_url + 'w342' + movie.poster_path}
+                        alt={movie.title}/>
+                )}
+                <h5>{movie.title}</h5>
+            </Link>
         </li>
     )
 }
