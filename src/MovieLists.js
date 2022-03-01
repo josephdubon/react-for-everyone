f20613d02d640f43dee082aa3a4fc1fa6da88b37
Joseph Dubon <josephdubon@pm.me>
2022-02-28 18:20:37 -0800

    implemented a filter/search to the movies list component

--- src/MovieLists.js
+++ src/MovieLists.js
@@ -1,13 +1,23 @@
-const movies = [{id: 1, name: 'Juice'}, {id: 2, name: 'Don\'t Be A Menace'}, {id: 3, name: 'A Goofy Movie'}]
+import {useState} from 'react'
+
+const movies = [{id: 1, name: 'Juice'}, {id: 2, name: 'Don\'t Be A Menace'}, {id: 3, name: 'A Goofy Movie'},]
 
 export function MoviesList() {
+    const [filter, setFilter] = useState('')
+
     return (<div>
+        {/* Search box */}
+        Filter: <input
+        type="text"
+        onChange={(e) => setFilter(e.target.value)} value={filter}
+    />
+
         <ul>
-            {movies.map((movie) => {
-                return (
-                    <li key={movie.id}>{movie.name}</li>
-                )
-            })}
+            {/* Change to lowercase to implement non-case sensitive filter */}
+            {movies.filter((movie) =>
+                movie.name.toLowerCase().includes(filter)).map((movie) =>
+                <li key={movie.id}>{movie.name}</li>)}
         </ul>
     </div>)
 }
+
